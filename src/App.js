46861cd300fe4ea2e1e0260f6d653be7eeafa53c
Dhairya Ostwal <50984984+dhairyaostwal@users.noreply.github.com>
2021-02-07 11:12:02 +0530

    Added style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import Saly17 from "../public/images/Saly17.png";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div></div>
+      <div className="App--Images">
+        <img src={Saly17} />
+      </div>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
