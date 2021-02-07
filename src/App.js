f79c443eacc0b260243c9e27b9eab1c909557b34
Dhairya Ostwal <50984984+dhairyaostwal@users.noreply.github.com>
2021-02-07 11:51:21 +0530

    Finished 🎉

--- src/App.js
+++ src/App.js
@@ -5,10 +5,36 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="App--Images">
-        <img src={Saly17} />
+      <h1 style={{ fontSize: "3rem" }}>Sorry 😢 </h1>
+      <div>
+        <img className="App--Images" src={Saly17} />
       </div>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2 style={{ fontSize: "1.4rem" }}>We're down for Maintenance 🛠</h2>
+      <p style={{ fontSize: "1.3rem", marginTop: "-1rem" }}>
+        Will be back soon :)
+      </p>
+      <div>
+        <h1>WTH is TaramaOS?</h1>
+        <p style={{ fontSize: "1.2rem", textAlign: "justify" }}>
+          TaramaOS is not just a usual project. It aims to make your favourite
+          restaurant even more efficient where all others algorithms like FCFS,
+          SRTF, SJF, Round Robin etc. fail. So, the project started as a simple
+          C++ program eventually turning into a web-app supported by Flask.
+        </p>
+      </div>
+
+      <div>
+        <h3>
+          Always available at{" "}
+          <a
+            href="https://dhairyaostwal.netlify.app/"
+            target="_blank"
+            style={{ color: "#5352ED" }}
+          >
+            @dhairyaostwal
+          </a>
+        </h3>
+      </div>
     </div>
   );
 }
